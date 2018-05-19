c50cd94592b86affa3205fca707c4fccb891d9b1
CodyEthanJordan <codyethanjordan@gmail.com>
2018-05-19 16:15:23 -0400

    Making fitbg function

---
+++ PyJose/fitbg.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+
+def fitbg(dataImage, x1, x2,):
+    dims = size(dataim)
+    nx = dims[1]
+    ny = dims[2]
+
+    bgdeg = 1
+    backgroundThreshold = 3
+    verbosityLevel = 0
+    plotType = [0,0,0,0]
+    gotovect = -1 #maybe replace with NONE
+    inmask = np.zeros(())
