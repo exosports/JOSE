f53ca1383f0c0a27ec2fc991e43fff8011fb1012
Cody Jordan <codyethanjordan@knights.ucf.edu>
2018-05-19 20:02:18 -0400

    checking astropy works

--- PyJose/tests/test_fitbg.py
+++ PyJose/tests/test_fitbg.py
@@ -1,8 +1,13 @@
 import unittest
+import numpy as np
+from astropy.io import fits as pyfits
+
 
 class test_fitbg(unittest.TestCase):
-    def test_A(self):
-        self.fail("Not implemented")
+    def test_fitsLoading(self):
+        '''Sanity check to ensure test data is present'''
+        data = pyfits.getdata('images/ex1.fits') #more of an integration test than unit tests, 
+
 
 if __name__ == '__main__':
     unittest.main()
