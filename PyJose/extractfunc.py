9e9b3af90d8f08608bfb12da5fd6edaaa7b31bec
Cody Jordan <codyethanjordan@knights.ucf.edu>
2018-05-23 16:33:29 -0400

    extractfunc and test data

---
+++ PyJose/extractfunc.py
@@ -0,0 +1,6 @@
+
+def extractfunc():
+    '''docstring'''
+    pass
+
+
---
+++ PyJose/tests/test_extractfunc.py
@@ -0,0 +1,12 @@
+import unittest
+import numpy as np
+import numpy.testing as npt
+
+
+
+class test_extractfunc(unittest.TestCase):
+    def test_A(self):
+        self.fail("Not implemented")
+
+if __name__ == '__main__':
+    unittest.main()
