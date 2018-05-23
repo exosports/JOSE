ba0e84ab3fd223ef1805bb44f4378b86559845b7
Cody Jordan <codyethanjordan@knights.ucf.edu>
2018-05-23 17:18:20 -0400

    Realized IDL functions use parameters as output, redoing test case for extractfunc and new data

--- PyJose/extractfunc.py
+++ PyJose/extractfunc.py
@@ -1,6 +1,24 @@
+import logging
+import numpy as np
 
-def extractfunc():
+# should these low level functions be done in C for performance?
+
+log = logging.getLogger(__name__)
+
+def extractfunc(xvals, datav, varv, profv, eval, coeffv):
     '''docstring'''
-    pass
+
+    #TODO: input validation with logging
+
+    goodPixels = profv != 0
+    if not np.any(goodPixels): #all pixels are bad
+        log.error('No good pixels in data, returning 0')
+        return 0, None
+    
+
+    opvar = 5
+    opt = 5
+
+    return opt, opvar
 
 
--- PyJose/tests/test_extractfunc.py
+++ PyJose/tests/test_extractfunc.py
@@ -2,11 +2,34 @@ import unittest
 import numpy as np
 import numpy.testing as npt
 
-
+from extractfunc import extractfunc
 
 class test_extractfunc(unittest.TestCase):
-    def test_A(self):
-        self.fail("Not implemented")
+    def test_IDLAgreement1(self):
+        coeffv = np.loadtxt('tests/testData/extractfuncData/coeffv.csv', delimiter=',')
+        datav = np.loadtxt('tests/testData/extractfuncData/datav.csv', delimiter=',')
+        eval = np.loadtxt('tests/testData/extractfuncData/eval.csv', delimiter=',')
+        opt = np.loadtxt('tests/testData/extractfuncData/opt.csv', delimiter=',')
+        opvar = np.loadtxt('tests/testData/extractfuncData/opvar.csv', delimiter=',')
+        profv = np.loadtxt('tests/testData/extractfuncData/profv.csv', delimiter=',')
+        varv = np.loadtxt('tests/testData/extractfuncData/varv.csv', delimiter=',')
+        xvals = np.loadtxt('tests/testData/extractfuncData/xvals.csv', delimiter=',')
+
+        results, opvarResult = extractfunc(xvals, datav, varv, profv, eval, coeffv)
+
+        npt.assert_allclose(results, opt)
+
+    def test_allBad(self):
+        xvals = np.array([1,2,3])
+
+        datav = np.linspace(0, 10)
+        varv = np.linspace(0, 10)
+        profv = np.zeros(size(varv)) #all bad pixels
+        eval = None
+        coeffv = None
+
+
+        results = extractfunc(xvals, datav, varv, profv, eval, coeffv, )
 
 if __name__ == '__main__':
     unittest.main()
