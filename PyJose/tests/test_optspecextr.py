763c86d74229b027e7f0d5201a0fb747549c0a63
CodyEthanJordan <codyethanjordan@gmail.com>
2018-05-30 00:39:57 -0400

    Adding text data for full regression test vs IDL jose

--- PyJose/tests/test_optspecextr.py
+++ PyJose/tests/test_optspecextr.py
@@ -1,12 +1,13 @@
 import unittest
 import numpy as np
+import numpy.testing as npt
 from astropy.io import fits as pyfits
 from optspecextr import optspectextr
 
 
 class test_optspecextr(unittest.TestCase):
-    def test_A(self):
-        frame1 = pyfits.open('images/ex1.fits')[0]
+    def test_fullIntegration_example1(self):
+        frame1 = pyfits.open('tests/testData/optspecextr/example1/ex1.fits')[0]
         Q = frame1.header.get('EPADU')
         rn = frame1.header.get('RDNOISE') / Q
 
@@ -14,6 +15,13 @@ class test_optspecextr(unittest.TestCase):
         rightBound = 270
 
         varim = np.abs(frame1.data) / Q + rn**2
+
+        optimal_spectrum = optspectextr()
+
+        opspec1 = np.loadtxt('tests/testData/optspecextr/example1/opspec1.csv', delimiter=',')
+
+        npt.assert_allclose(optimal_spectrum, opspec1)
+
 
 
 if __name__ == '__main__':
