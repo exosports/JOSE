f11416a9de50ecc0eda234c6e339d1ca12c70c73
CodyEthanJordan <codyethanjordan@gmail.com>
2018-06-06 00:07:12 -0400

    Cleaning up project and tests after rearranging project structure

--- PyJose/jose/test/test_stdextr.py
+++ PyJose/jose/test/test_stdextr.py
@@ -1,25 +1,27 @@
 import unittest
 import numpy as np
 import numpy.testing as npt
+import os
 import sys
-print("Here are the paths")
-print(sys.path)
-print('---------------')
 
-from stdextr import stdextr
+import jose
+
+testData_directory = os.path.join(os.path.dirname(__file__), 'testData', 'stdextrData')
 
 class test_stdextr(unittest.TestCase):
     def test_IDLOracle_regression1(self):
-        x1 = np.loadtxt('tests/testData/stdextrData/x1.csv', delimiter=',')
-        x2 = np.loadtxt('tests/testData/stdextrData/x2.csv', delimiter=',')
-        dataim = np.loadtxt('tests/testData/stdextrData/dataim.csv', delimiter=',')
-        stdspec = np.loadtxt('tests/testData/stdextrData/stdspec.csv', delimiter=',')
-        stdvar = np.loadtxt('tests/testData/stdextrData/stdvar.csv', delimiter=',')
-        varim = np.loadtxt('tests/testData/stdextrData/varim.csv', delimiter=',')
+        x1 = np.loadtxt(os.path.join(testData_directory, "x1.csv"), delimiter=',')
+        x2 = np.loadtxt(os.path.join(testData_directory, "x2.csv"), delimiter=',')
+        dataim = np.loadtxt(os.path.join(testData_directory, "dataim.csv"), delimiter=',')
+        stdspec = np.loadtxt(os.path.join(testData_directory, "stdspec.csv"), delimiter=',')
+        stdvar = np.loadtxt(os.path.join(testData_directory, "stdvar.csv"), delimiter=',')
+        varim = np.loadtxt(os.path.join(testData_directory, "varim.csv"), delimiter=',')
         x1 = int(np.asscalar(x1))
-        x2 = int(np.asscalar(x2)) + 1
+        x2 = int(np.asscalar(x2)) + 1 #IDL is inclusive in the last index, numpy/python is not
 
-        output_spectrum, output_var = stdextr(dataim, varim, (x1, x2))
+        print(dir(jose))
+
+        output_spectrum, output_var = jose.stdextr(dataim, varim, (x1, x2))
 
         npt.assert_allclose(output_spectrum, stdspec, rtol=0.1)
         npt.assert_allclose(output_var, stdvar, rtol=0.1)
