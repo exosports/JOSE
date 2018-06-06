edc28d0667f8f6efd55c56db6c78255009a79aa9
CodyEthanJordan <codyethanjordan@gmail.com>
2018-06-06 00:46:47 -0400

    Fixing more imports and module structure

--- PyJose/jose/__init__.py
+++ PyJose/jose/__init__.py
@@ -1,1 +1,6 @@
 from jose.stdextr import stdextr
+from jose.fitbg import fitbg
+from jose.extractfunc import extractfunc
+from jose.optspecextr import optspectextr
+from jose.polyfunc import polyfunc
+from jose.procvect import procvect
--- PyJose/jose/test/test_fitbg.py
+++ PyJose/jose/test/test_fitbg.py
@@ -2,34 +2,35 @@ import unittest
 import numpy as np
 import numpy.testing as npt
 from astropy.io import fits as pyfits
+import os
 
-from fitbg import fitbg
+import jose
 
+testData_directory = os.path.join(os.path.dirname(__file__), 'testData', 'fitbgData')
 
 class test_fitbg(unittest.TestCase):
     def test_testDataPresent(self):
         '''Sanity check to ensure test data is present'''
-        data = pyfits.open('images/ex1.fits') #more of an integration test than unit tests, 
-        dataim = np.loadtxt('tests/testData/fitbgData/dataim.csv', delimiter=',')
-        x1 = np.loadtxt('tests/testData/fitbgData/x1.csv', delimiter=',')
-        x2 = np.loadtxt('tests/testData/fitbgData/x2.csv', delimiter=',')
-        bgdeg = np.loadtxt('tests/testData/fitbgData/bgdeg.csv', delimiter=',')
-        bgmask = np.loadtxt('tests/testData/fitbgData/bgmask.csv', delimiter=',')
-        bgres = np.loadtxt('tests/testData/fitbgData/bgres.csv', delimiter=',')
-        bthresh = np.loadtxt('tests/testData/fitbgData/bthresh.csv', delimiter=',')
-        errvect = np.loadtxt('tests/testData/fitbgData/errvect.csv', delimiter=',')
-        gotovect = np.loadtxt('tests/testData/fitbgData/gotovect.csv', delimiter=',')
-        inmask = np.loadtxt('tests/testData/fitbgData/inmask.csv', delimiter=',')
-        plottype = np.loadtxt('tests/testData/fitbgData/plottype.csv', delimiter=',')
-        q = np.loadtxt('tests/testData/fitbgData/q.csv', delimiter=',')
-        skyvar = np.loadtxt('tests/testData/fitbgData/skyvar.csv', delimiter=',')
-        varim = np.loadtxt('tests/testData/fitbgData/varim.csv', delimiter=',')
-        verbose = np.loadtxt('tests/testData/fitbgData/verbose.csv', delimiter=',')
-        v0 = np.loadtxt('tests/testData/fitbgData/v0.csv', delimiter=',')
+        dataim = np.loadtxt(os.path.join(testData_directory, "dataim.csv"), delimiter=',')
+        x1 = np.loadtxt(os.path.join(testData_directory, "x1.csv"), delimiter=',')
+        x2 = np.loadtxt(os.path.join(testData_directory, "x2.csv"), delimiter=',')
+        bgdeg = np.loadtxt(os.path.join(testData_directory, "bgdeg.csv"), delimiter=',')
+        bgmask = np.loadtxt(os.path.join(testData_directory, "bgmask.csv"), delimiter=',')
+        bgres = np.loadtxt(os.path.join(testData_directory, "bgres.csv"), delimiter=',')
+        bthresh = np.loadtxt(os.path.join(testData_directory, "bthresh.csv"), delimiter=',')
+        errvect = np.loadtxt(os.path.join(testData_directory, "errvect.csv"), delimiter=',')
+        gotovect = np.loadtxt(os.path.join(testData_directory, "gotovect.csv"), delimiter=',')
+        inmask = np.loadtxt(os.path.join(testData_directory, "inmask.csv"), delimiter=',')
+        plottype = np.loadtxt(os.path.join(testData_directory, "plottype.csv"), delimiter=',')
+        q = np.loadtxt(os.path.join(testData_directory, "q.csv"), delimiter=',')
+        skyvar = np.loadtxt(os.path.join(testData_directory, "skyvar.csv"), delimiter=',')
+        varim = np.loadtxt(os.path.join(testData_directory, "varim.csv"), delimiter=',')
+        verbose = np.loadtxt(os.path.join(testData_directory, "verbose.csv"), delimiter=',')
+        v0 = np.loadtxt(os.path.join(testData_directory, "v0.csv"), delimiter=',')
+
 
     def test_testIDLAgreement_fitbg_loose(self):
         '''Recorded data from first part of IDL jose tutorial to compare against'''
-        data = pyfits.open('images/ex1.fits') #more of an integration test than unit tests, 
         dataim = np.loadtxt('tests/testData/fitbgData/dataim.csv', delimiter=',')
         x1 = np.loadtxt('tests/testData/fitbgData/x1.csv', delimiter=',')
         x2 = np.loadtxt('tests/testData/fitbgData/x2.csv', delimiter=',')
@@ -51,7 +52,7 @@ class test_fitbg(unittest.TestCase):
         bpct = None
         nobgfit = None
 
-        results = fitbg(dataim, (int(np.asscalar(x1)), int(np.asscalar(x2))), varim)
+        results = jose.fitbg(dataim, (int(np.asscalar(x1)), int(np.asscalar(x2))), varim)
 
         bgim = np.loadtxt('tests/testData/fitbgData/bgim.csv', delimiter=',')
 
@@ -64,7 +65,7 @@ class test_fitbg(unittest.TestCase):
         variance = np.ones(np.shape(data))
         object_bounds = (80, 120)
 
-        background_image = fitbg(data, object_bounds, variance)
+        background_image = jose.fitbg(data, object_bounds, variance)
 
         npt.assert_allclose(background_image, np.zeros(np.shape(data)))
 
@@ -74,7 +75,7 @@ class test_fitbg(unittest.TestCase):
         variance = np.ones(np.shape(data))
         object_bounds = (80, 120)
 
-        background_image = fitbg(data, object_bounds, variance)
+        background_image = jose.fitbg(data, object_bounds, variance)
 
         npt.assert_allclose(background_image, np.ones(np.shape(data)))
 
