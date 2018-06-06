ebee433940f8d9c8edb7538b5244c7659a1aa64a
Cody Jordan <codyethanjordan@knights.ucf.edu>
2018-06-06 17:16:52 -0400

    Backing up changes for new profile function

--- PyJose/jose/create_profile.py
+++ PyJose/jose/create_profile.py
@@ -1,7 +1,16 @@
 import numpy as np
 
+from .procvect import procvect
+
 def create_profile(data):
     profile = np.zeros(np.shape(data))
+
+    
+    for i in range(np.shape(profile)[1]):
+        profile[:,i] = procvect( list(range(len(profile[:,i]))),
+                                data[:, i],
+
+            )
 
     # enforce positivity
     profile[profile < 0] = 0
