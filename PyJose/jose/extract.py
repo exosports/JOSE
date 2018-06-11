603514434fcd1a1fadf4a5c35087a969532e2584
Cody Jordan <codyethanjordan@knights.ucf.edu>
2018-06-11 13:08:53 -0400

    extract function

--- PyJose/jose/extract.py
+++ PyJose/jose/extract.py
@@ -1,11 +1,11 @@
 import numpy as np
 
-def extract(sky_subtracted, revised_variance, profile, standard_spectrum):
-    threshold = 5
+def extract(sky_subtracted, revised_variance, profile):
+    threshold = 25
     mask = np.full(np.shape(sky_subtracted), True) #initialize to all good pixels
     converged = False
-    previous_mask = np.copy(mask) # could make more effecient just by taking sum?
     maximum_rejects = 1
+    previous_bad_pixels = np.full(np.shape(sky_subtracted), False)
 
     while not converged:
         denominator = np.sum(mask * profile * profile / revised_variance, axis=1)
@@ -13,12 +13,16 @@ def extract(sky_subtracted, revised_variance, profile, standard_spectrum):
         
         SDR = (sky_subtracted - spectrum * profile) / revised_variance
 
+        bad_pixels = SDR > threshold
 
-    
-    mask = (sky_subtracted - standard_spectrum * profile)**2 <= revised_variance * threshold**2
+        # flattened indexes of maximum_reject largest outliers
+        largest_SDR = np.argpartition(SDR, SDR.size-maximum_rejects, axis=None)[-maximum_rejects:]
 
+        if np.array_equal(bad_pixels, previous_bad_pixels):
+            converged = True
+        else:
+            previous_bad_pixels = np.copy(bad_pixels)
+            mask[largest_SDR // mask.shape[0], largest_SDR % mask.shape[1]] = False
+            mask = np.logical_or(mask, np.logical_not(bad_pixels))
 
-
-
-
-    return None
+    return spectrum
