c85c6acbe332e4a12448c75f7a9ce2a24753aa3e
Cody Jordan <codyethanjordan@knights.ucf.edu>
2018-06-13 16:32:29 -0400

    making command line stuff

--- PyJose/jose/PyJose.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import logging
-
-from astropy.io import fits as pyfits
-
-
-
-if __name__ == "__main__":
-    pass 
---
+++ PyJose/jose/jose.py
@@ -0,0 +1,13 @@
+import numpy as np
+import logging
+import argparse
+from astropy.io import fits as pyfits
+
+import jose
+
+log = logging.getLogger('jose')
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='')
+    print('yoooo')
+    pass 
