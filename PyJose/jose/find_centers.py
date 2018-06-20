890cb20bf300836d3fb65522adadcbd1e86f044e
CodyEthanJordan <codyethanjordan@gmail.com>
2018-06-19 21:53:08 -0400

    Adding initial value guesses to guass fit

--- PyJose/jose/find_centers.py
+++ PyJose/jose/find_centers.py
@@ -1,5 +1,5 @@
 import numpy as np
-from scipy.optimize import curve_fit
+from scipy.optimize import curve_fit, least_squares
 
 import matplotlib.pyplot as plt
 
@@ -7,11 +7,13 @@ def find_centers(data):
     '''Returns a value for the center for each row of input data'''
     centers = np.zeros(data.shape[0])
 
-    gaussian = lambda x, a, b, c: a * np.exp(-(x - b)**2 / c**2)
+    gaussian = lambda x, a, b, c, d: a * np.exp(-(x - b)**2 / c**2) + d
+
 
     x = list(range(len(data[0])))
     for i, row in enumerate(data):
-        popt, pcov = curve_fit(gaussian, x, row, loss='soft_l1')
+        # make own function? unit test?
+        popt, pcov = curve_fit(gaussian, x, row, p0=(np.max(row), len(x)/2, 1, np.min(row)))
         centers[i] = popt[1]
         plt.plot(x, row)
         plt.plot(x, gaussian(x, *popt))
