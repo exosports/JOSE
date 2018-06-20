0547901540485c2ab1c29edb79140bba066677c9
CodyEthanJordan <codyethanjordan@gmail.com>
2018-06-20 01:08:47 -0400

    Robust fitting scipy cook book implemented on find_centers

--- PyJose/jose/find_centers.py
+++ PyJose/jose/find_centers.py
@@ -9,14 +9,26 @@ def find_centers(data):
 
     gaussian = lambda x, a, b, c, d: a * np.exp(-(x - b)**2 / c**2) + d
 
+    f = lambda params, x, y: gaussian(x, *params) - y
+
 
     x = list(range(len(data[0])))
     for i, row in enumerate(data):
+        initial_parameters = (np.max(row), len(x) / 2, 1, np.min(row)) # create reasonable initial guesses
         # make own function? unit test?
-        popt, pcov = curve_fit(gaussian, x, row, p0=(np.max(row), len(x)/2, 1, np.min(row)))
-        centers[i] = popt[1]
-        plt.plot(x, row)
-        plt.plot(x, gaussian(x, *popt))
+        optimization = least_squares(f, initial_parameters, loss='soft_l1', f_scale=0.1, args=(x, row))
+        results = gaussian(x, *optimization.x)
+
+        plt.plot(x, row, x, results)
         plt.show()
+
+
+
+
+        #popt, pcov = curve_fit(gaussian, x, row, p0=(np.max(row), len(x)/2, 1, np.min(row)))
+        #centers[i] = popt[1]
+        #plt.plot(x, row)
+        #plt.plot(x, gaussian(x, *popt))
+        #plt.show()
 
     return centers
