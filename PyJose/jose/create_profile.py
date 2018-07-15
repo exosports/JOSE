4db4062d776fdb0802356428810180f63ee0a762
CodyEthanJordan <codyethanjordan@gmail.com>
2018-07-14 22:10:25 -0400

    Profile fitting

--- PyJose/jose/create_profile.py
+++ PyJose/jose/create_profile.py
@@ -11,26 +11,7 @@ def create_profile(data, sky, variance):
 
     x_values = np.array(list(range(len(profile_estimate[:,0]))))
     for i in range(np.shape(profile_estimate)[1]):
-        fit_complete = False
-        mask = np.full(np.shape(profile_estimate[:,i]), True)
-        previous_outlieres = np.full(np.shape(profile_estimate[:,i]), False)
-        while not fit_complete:
-            coeff = np.polyfit(x_values[mask],
-                               profile_estimate[:,i][mask], 
-                               deg = 3,
-                               w = 1 / variance[:,i][mask])
-            model = np.poly1d(coeff)
-
-            fitted_profile = model(x_values)
-
-            SNR = (data[:,i] - sky[:,i] - f * fitted_profile)**2 / variance[:,i]
-            outliers = SNR > rejection_threshold
-            if np.array_equal(previous_outlieres, outliers):
-                fit_complete = True
-                profile[:,i] = fitted_profile
-            else:
-                previous_outlieres = np.copy(outliers)
-                mask[outliers] = False
+        profile[:, i], mask, model = procvect(x_values, data[:, i], variance[:, i], 9, "polynomial", False, {'deg' : 3})
 
     # enforce positivity
     profile[profile < 0] = 0
